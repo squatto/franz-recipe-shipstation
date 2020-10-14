c42caec6b5cbc01d57966d9e0512a3bc7ce9ab4c
Scott Carpenter <scott@payforstay.com>
2020-10-14 12:25:25 -0600

    Support the old _and_ the new ShipStation UI

--- webview.js
+++ webview.js
@@ -12,7 +12,30 @@ module.exports = Franz => {
     let unread = 0;
 
     try {
-      unread = parseInt(
+      // old layout
+      unread += parseInt(
+        contains('#workflow span.tray-display-name', 'Awaiting Shipment')[0]
+          .parentElement
+          .querySelector('.status-count')
+          .textContent
+          .replace(',', ''),
+        10,
+      );
+      unread += parseInt(
+        contains('#workflow span.tray-display-name', 'On Hold')[0]
+          .parentElement
+          .querySelector('.status-count')
+          .textContent
+          .replace(',', ''),
+        10,
+      );
+    } catch (e) {
+      // unable to find order count (it's likely not the old layout)
+    }
+
+    try {
+      // new layout
+      unread += parseInt(
         contains('#app-root a', 'Awaiting Shipment')[0]
           .nextSibling
           .children[0]
@@ -29,7 +52,7 @@ module.exports = Franz => {
         10,
       );
     } catch (e) {
-      // unable to find order count
+      // unable to find order count (it's likely not the new layout)
     }
 
     Franz.setBadge(parseInt(unread, 10));
